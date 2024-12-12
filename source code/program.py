bdc9fbb09f7ac1dc0b8f39e3ba40a94402f29e9e
Duong Chi Thanh <113772677+thanhdx23@users.noreply.github.com>
2024-12-12 13:59:57 +0700

    Update program.py

--- source code/program.py
+++ source code/program.py
@@ -5,11 +5,9 @@ from create_video import create_video
 from moviepy.config import change_settings
 
 
-
-
 if __name__ == "__main__":
     change_settings({"IMAGEMAGICK_BINARY": "C:/Program Files/ImageMagick-7.1.1-Q16-HDRI/magick.exe"})
-    #Đường dẫn tới thư mục chứa ảnh và tệp âm thanh
+    
     url = "https://thanhnien.vn/chu-tich-tphcm-neu-huong-xu-ly-hon-1000-nha-dat-cong-bo-trong-185241210091326944.htm"
 
     summary = summarize_article_from_url(url)
@@ -19,9 +17,9 @@ if __name__ == "__main__":
     main_images_url = get_main_images_from_article(url)
     download_images(main_images_url, "Image")
 
-    image_folder = "Image"  # Thay bằng đường dẫn tới thư mục chứa ảnh
-    audio_file = "faster_output.mp3"  # Thay bằng đường dẫn tới tệp âm thanh
-    output_file = "output_video.mp4"  # Tên tệp video đầu ra
-    title_text, _ = get_article_from_url(url)  # Tiêu đề của video
-
+    image_folder = "Image"
+    audio_file = "faster_output.mp3"  
+    output_file = "output_video.mp4" 
+    title_text, _ = get_article_from_url(url) 
+    
     create_video(image_folder, audio_file, output_file, title_text)
